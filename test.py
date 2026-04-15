b0c31a47debb955a8b11de6f8acbd3b8261289d7
tran986 <tran.986@buckeyemail.osu.edu>
2026-04-15 10:05:47 -0400

    2nd challenge

--- test.py
+++ test.py
@@ -1,24 +1,22 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-"""
-class Solution(object):
-    def twoSum(self, nums, target):
-        #:type nums: List[int]
-        #:type target: int
-        #:rtype: List[int]
-        nums = input("type nums: ")
-        target = input("type target: ")
-s = Solution()
-print(s.twoSum(4,2))
-"""
-      
+
 nums = [2,7,11]
-target = 9
+target = 18
 
 id=1
-for i in range(len(nums)):
-    n=nums[i] 
-    id = id + 1
-    print(n + nums[id])
-    #print(f"idx {i} - {n}")
-    
-        
+for i in range(len(nums)): #index of 1st num
+    for j in range(i + 1): #index of 2nd num
+       if nums[i] + nums[j] == target:
+           print([i, j])
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+x = -12345
+digit = [int(d) for d in str(abs(x))]
+digit_rev = digit[::-1]
+if digit == digit_rev :
+    print("True")
+else:
+    print("False")
+
+
+     
