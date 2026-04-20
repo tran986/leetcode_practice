aebfaf47fabe30a18dedb739d2de25b27f53e546
tran986 <tran.986@buckeyemail.osu.edu>
2026-04-19 22:55:46 -0400

    happy num

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ in1_map=int("".join(map(str, in1_r)))
 in2_map=int("".join(map(str, in2_r)))
 result=in1_map + in2_map
 print([int(d) for d in reversed(str(result))])
-"""
+
 
 #Write a function to find the longest common prefix string amongst an array of strings.
 #If there is no common prefix, return an empty string "".
@@ -65,22 +65,31 @@ for i in range(min_len):
         prefix += char      # only adds char if no mismatch
 
 print(prefix)
-        
+"""
 
+#Input: n = 19
+#Output: true
+#Explanation:
+#1^2 + 9^2 = 82
+#8^2 + 2^2 = 68
+#6^2 + 8^2 = 100
+#1^2 + 0^2 + 0^2 = 1 -> happy number, return true
+
+n=13
+seen = set()
+while n != 1:
+    if n in seen:
+        return False
+        break
+    seen.add(n)
+    digit_sum = []
+    for digit in str(n):
+        square = int(digit) ** 2
+        digit_sum.append(square)
+    n = sum(digit_sum)
+
+print(True)
 
     
-    
-        
 
-"""
-res = []
-for w in word:
-    chars = []
-    for char in w:
-        chars.append(char)
-    res.append(chars)
-
-for i in res:
-    print(i)
-print(i[0])
-"""
+           
